929d17b7936c900a90649e2b743d3d6c7185969c
devbobi <devbobi505@gmail.com>
2023-01-26 03:28:46 +0600

    table initiated

---
+++ src/components/Table/Tables.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Table } from "reactstrap";
+
+const Tables = ({ children }) => {
+  return (
+    <>
+      <Table striped hover>
+        <thead>
+          <tr>
+            <th>
+              {" "}
+              <i class="fa-solid fa-arrow-up-long"></i>
+              <i class="fa-solid fa-arrow-down-long mr-2"></i>Name
+            </th>
+            <th>Modified by</th>
+            <th>Last modified</th>
+            <th>File size</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Devs team salary</td>
+            <td>Abdullah al rafee</td>
+            <td>Jun, 03, 2022</td>
+            <td>320kb</td>
+          </tr>
+          <tr>
+            <td>Ops team salary</td>
+            <td>Nurul Haque</td>
+            <td>Jun, 03, 2022</td>
+            <td>320kb</td>
+          </tr>
+          <tr>
+            <td>Ops team salary</td>
+            <td>Nurul Haque</td>
+            <td>Jun, 03, 2022</td>
+            <td>320kb</td>
+          </tr>
+          <tr>
+            <td>Ops team salary</td>
+            <td>Nurul Haque</td>
+            <td>Jun, 03, 2022</td>
+            <td>320kb</td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
+  );
+};
+
+export default Tables;
---
+++ src/components/Table/docs/Table.stories.jsx
@@ -0,0 +1,10 @@
+import Tables from "../Tables";
+
+export default {
+  title: "components/Table",
+  component: Tables,
+};
+
+const Template = (args) => <Tables {...args} />;
+export const Default = Template.bind({});
+Default.args = {};
