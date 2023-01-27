b8ef2e27a2df57c467d59f2200c276e9b08ddd5b
saminravi99 <saminisrar1@gmail.com>
2023-01-28 00:26:18 +0600

    fix: link btn show code

--- src/components/Buttons/LinkBtn.jsx
+++ src/components/Buttons/LinkBtn.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "../Buttons/BtnBase";
 
-const LinkBtn = () => {
+const LinkBtn = ({}) => {
   return (
     <>
       <Button color="link">Default Link Button</Button>
