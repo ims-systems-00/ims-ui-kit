22db880d1cba2592401409eebadcdde68bec3c83
saminravi99 <saminisrar1@gmail.com>
2023-02-06 22:15:06 +0600

    WIP

---
+++ src/components/Forms/ImsSelectInput/ReactSelectBase.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReactSelectBase = () => {
+  return (
+    <div>ReactSelectBase</div>
+  )
+}
+
+export default ReactSelectBase
---
+++ src/components/Forms/docs/ImsSelectInput.stories.js
@@ -0,0 +1,9 @@
+import ReactSelect from "../ImsSelectInput/ReactSelectBase";
+
+export default {
+  title: "components/ReactSelect",
+  component: ReactSelect,
+};
+const Template = (args) => <ReactSelect {...args} />;
+export const ImsSelect = Template;
+ImsSelect.args = {};
