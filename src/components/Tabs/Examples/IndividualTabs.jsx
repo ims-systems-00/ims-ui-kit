2b4e172a7ab9d1ad7aac2fee9488982e3b4e9c58
saminravi99 <saminisrar1@gmail.com>
2023-02-07 20:01:33 +0600

    add: Tabs

---
+++ src/components/Tabs/Examples/IndividualTabs.jsx
@@ -0,0 +1,54 @@
+import Nav from "../NavBase";
+import NavItem from "../NavItemBase";
+import NavLink from "../NavLinkBase";
+
+const IndividualTabs = ({}) => {
+  return (
+    <>
+      <h1>Individual Tab Type 1</h1>
+      <Nav tabs individual type="1">
+        <NavItem>
+          <NavLink
+            leftIcon="fa-solid fa-home"
+            active
+            link
+            onClick={(e) => e.preventDefault()}
+          >
+            Home
+          </NavLink>
+        </NavItem>
+      </Nav>
+      <br /> <br />
+      <h1>Individual Tab Type 2</h1>
+      <Nav tabs individual type="2">
+        <NavItem>
+          <NavLink
+            rightIcon="fa-solid fa-envelope"
+            active
+            link
+            onClick={(e) => e.preventDefault()}
+          >
+            Message
+          </NavLink>
+        </NavItem>
+      </Nav>
+      <br /> <br />
+      <h1>Individual Tab Type 3</h1>
+      <Nav tabs individual type="3">
+        <NavItem>
+          <NavLink
+            leftIcon="fa-solid fa-cog"
+            rightIcon="fa-solid fa-angle-down"
+            active
+            link
+            onClick={(e) => e.preventDefault()}
+          >
+            Settings
+          </NavLink>
+        </NavItem>
+      </Nav>
+    </>
+  );
+};
+
+export default IndividualTabs;
