c669b8861fa1eba890c64858e0234301db58a9be
saminravi99 <saminisrar1@gmail.com>
2023-02-12 19:54:30 +0600

    add: controlled date time

--- src/components/Forms/DateTimePicker/Examples/DefaultDateTime.jsx
+++ src/components/Forms/DateTimePicker/Examples/DefaultDateTime.jsx
@@ -1,11 +1,30 @@
-import DateTime from '../DateTimeBase'
+import { useState } from "react";
+import DateTime from "../DateTimeBase";
 
 const DefaultDateTime = ({}) => {
+  
   return (
     <>
-    <DateTime/>
+      <h4>Default</h4>
+      <DateTime />
+      <br /> <br />
+      <h4>With Placeholder</h4>
+      <DateTime inputProps={{ placeholder: "Select Date and Time" }} />
+      <br /> <br />
+      <h4>Full Disabled</h4>
+      <DateTime
+        inputProps={{
+          placeholder: "Select Date and Time",
+          disabled: true,
+          required: true,
+          name: "date",
+        }}
+      />
+      <br /> <br />
+      <h4>Controlled</h4>
+     
     </>
-  )
-}
+  );
+};
 
-export default DefaultDateTime
+export default DefaultDateTime;
--- src/components/Forms/DateTimePicker/docs/DateTime.stories.js
+++ src/components/Forms/DateTimePicker/docs/DateTime.stories.js
@@ -11,3 +11,5 @@ DateTimePicker.args = {};
 export { default as DefaultDateTime } from "../Examples/DefaultDateTime";
 export { default as DateTimeWithoutTime } from "../Examples/DateTimeWithoutTime";
 export { default as DisabledDates } from "../Examples/DisabledDates";
+export { default as ControlledDateTime } from "../Examples/ControlledDateTime";
+
