54f720cd5b9ae7d9cac07e2d22d6436dba1fcf00
saminravi99 <saminisrar1@gmail.com>
2023-02-13 18:49:35 +0600

    fix: unnecessary inputs

--- src/components/Forms/TextInput/ImsInputText.jsx
+++ src/components/Forms/TextInput/ImsInputText.jsx
@@ -1,7 +1,7 @@
-import classNames from "classnames";
-import React from "react";
-import { Col, FormGroup, Input, Label, Row } from "reactstrap";
 import PropTypes from "prop-types";
+import React from "react";
+import { Col, FormFeedback, FormGroup, Label } from "reactstrap";
+import Input from "../TextInput/InputBase";
 
 let ImsInputTextProps = Object.assign({}, Input.prototype.props);
 
@@ -11,29 +11,17 @@ let ImsInputTextProps = Object.assign({}, Input.prototype.props);
  */
 
 const ImsInputText = ({
-  leftIcon = "",
-  rightIcon = "",
-  leftIconStyle = "text-secondary",
-  rightIconStyle = "text-secondary",
   label,
-  type = "text",
   mandatory = false,
   error = "",
-  valid = false,
-  onChange,
   labelCol = "12",
   inputCol = "12",
-  value,
+  feedbackText = "",
   ...rest
 }) => {
-  const textInputRef = React.useRef(null);
-  const [focused, setFocused] = React.useState(false);
-  const handleFocus = (value) => {
-    setFocused(value);
-  };
   return (
     <>
-      <Row>
+      <FormGroup row>
         {label && (
           <Col sm={labelCol}>
             <Label>
@@ -42,52 +30,10 @@ const ImsInputText = ({
           </Col>
         )}
         <Col sm={inputCol}>
-          <FormGroup>
-            <div
-              ref={textInputRef}
-              onClick={() => {
-                textInputRef.current.querySelector("input").focus();
-                console.log("clicked");
-              }}
-              className={classNames(
-                "ims-input d-flex align-items-center px-2",
-                {
-                  "ims-input-focused": focused,
-                  "ims-input-error": error,
-                  "ims-input-success": valid && !error,
-                }
-              )}
-            >
-              {leftIcon && (
-                <i
-                  className={classNames(`
-                ${leftIcon}
-                ${leftIconStyle}
-                `)}
-                ></i>
-              )}
-              <Input
-                className="border border-0 ims-text-input px-2"
-                onChange={(e) => onChange(e)}
-                onFocus={() => handleFocus(true)}
-                onBlur={() => handleFocus(false)}
-                type={type}
-                value={value}
-                {...rest}
-              />
-              {rightIcon && (
-                <i
-                  className={classNames(`
-                  ${rightIcon}
-                  ${rightIconStyle}
-                  `)}
-                ></i>
-              )}
-            </div>
-            {error && <label className="text-danger">{error}</label>}
-          </FormGroup>
+          <Input invalid={error} {...rest} />
+          {error && <label className="text-danger">{error}</label>}
         </Col>
-      </Row>
+      </FormGroup>
     </>
   );
 };
