0c063c7a74d008166090847e1d7cc932d472c024
saminravi99 <saminisrar1@gmail.com>
2023-02-14 15:42:18 +0600

    add: proptypes

--- src/components/Pagination/PaginationLinkBase.jsx
+++ src/components/Pagination/PaginationLinkBase.jsx
@@ -2,6 +2,13 @@ import classNames from "classnames";
 import React from "react";
 import { PaginationLink } from "reactstrap";
 
+let PaginationLinkBaseProps = Object.assign({}, PaginationLink.prototype.props);
+/**
+ *
+ * @param {PaginationLinkBaseProps} props
+ * @returns
+ */
+
 const PaginationLinkBase = ({ children, link, ...rest }) => {
   if (link) {
     //in case of react router, we need to send Link tag as children
@@ -9,6 +16,10 @@ const PaginationLinkBase = ({ children, link, ...rest }) => {
   } else {
     return <PaginationLink {...rest}>{children}</PaginationLink>;
   }
+};
+
+PaginationLinkBase.propTypes = {
+  ...(PaginationLink.propTypes && PaginationLink.propTypes),
 };
 
 export default PaginationLinkBase;
