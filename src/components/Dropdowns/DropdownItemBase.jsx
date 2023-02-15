da502e215706fea90638ccd1cf21fe045dab532b
RiadHossain43 <riadhossain7464@gmail.com>
2023-02-15 20:04:00 +0600

    new commit from design systen

--- src/components/Dropdowns/DropdownItemBase.jsx
+++ src/components/Dropdowns/DropdownItemBase.jsx
@@ -7,22 +7,8 @@ let DropdownItemBaseProps = Object.assign({}, DropdownItem.prototype.props);
  * @param {DropdownItemBaseProps} props
  * @returns
  */
-const DropdownItemBase = ({
-  leftIcon = "",
-  rightIcon = "",
-  leftIconStyle = "text-secondary",
-  rightIconStyle = "text-secondary",
-  ...props
-}) => {
-  return (
-    <DropdownItem {...props} className="d-flex justify-content-between align-items-center">
-      <span>
-        {leftIcon && <i className={`${leftIcon} ${leftIconStyle} me-2`} />}
-        {props.children}
-      </span>
-      {rightIcon && <i className={`${rightIcon} ${rightIconStyle}`} />}
-    </DropdownItem>
-  );
+const DropdownItemBase = ({ ...props }) => {
+  return <DropdownItem {...props}>{props.children}</DropdownItem>;
 };
 
 DropdownItemBase.propTypes = {
