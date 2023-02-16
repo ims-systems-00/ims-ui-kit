a104797315e8935dac6c5bd4e9cfda75b5852469
saminravi99 <saminisrar1@gmail.com>
2023-02-16 20:40:18 +0600

    remove: unused variables removed

--- src/components/Forms/ImsSelectInput/Examples/SelectWithIcon.jsx
+++ src/components/Forms/ImsSelectInput/Examples/SelectWithIcon.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-import ReactSelect from "../ReactSelectBase";
 import InputGroup from "components/Forms/TextInput/InputGroupBase";
 import InputGroupText from "components/Forms/TextInput/InputGroupTextBase";
+import ReactSelect from "../ReactSelectBase";
 import options from "./options.js";
-import InputBase from "components/Forms/TextInput/InputBase";
 
 const SelectWithIcon = () => {
   return (
