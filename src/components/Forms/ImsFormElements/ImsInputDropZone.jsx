40b34a130e3ed256298c4d899d6239687ab96d16
saminravi99 <saminisrar1@gmail.com>
2023-02-17 11:29:41 +0600

    add: ims form elements

---
+++ src/components/Forms/ImsFormElements/ImsInputDropZone.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ImsInputDropzone = () => {
+  return (
+    <div>ImsInputDropzone</div>
+  )
+}
+
+export default ImsInputDropzone
---
+++ src/components/Forms/ImsFormElements/ImsTextEditor.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ImsTextEditor = () => {
+  return (
+    <div>ImsTextEditor</div>
+  )
+}
+
+export default ImsTextEditor
