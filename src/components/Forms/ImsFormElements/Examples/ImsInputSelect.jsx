8e7f568e01b4f6be18ae2e7ac55f1d2570d7019c
saminravi99 <saminisrar1@gmail.com>
2023-02-17 18:52:13 +0600

    add: input text padding

--- src/components/Forms/ImsFormElements/Examples/ImsInputSelect.jsx
+++ src/components/Forms/ImsFormElements/Examples/ImsInputSelect.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import options from "components/Forms/ImsSelectInput/Examples/options";
 import ImsInputSelectBase from "../ImsInputSelect";
+import ImsInputSelectWithIcon from "../ImsInputSelectWithIcon";
 
-const ImsInputSelect = () => {
+const ImsInputSelect = ({}) => {
   return (
     <>
+      <h2>Without Icon</h2>
       <ImsInputSelectBase
         label="Label Text"
         onChange={(e) => console.log(e)}
@@ -12,6 +14,15 @@ const ImsInputSelect = () => {
           { value: "2", label: "Option 2" },
           { value: "3", label: "Option 3" },
         ]}
+        defaultOpt="Select Option"
+      />
+      <br /> <br />
+      <h2>With Icon</h2>
+      <ImsInputSelectWithIcon
+        label="Label Text"
+        icon="fa-solid fa-home"
+        onChange={(e) => console.log(e)}
+        options={options}
         defaultOpt="Select Option"
       />
     </>
---
+++ src/components/Forms/ImsFormElements/ImsInputSelectWithIcon.jsx
@@ -0,0 +1,58 @@
+import Select from "../ImsSelectInput/ReactSelectBase";
+import { Col, FormGroup, Label } from "reactstrap";
+import InputGroup from "../TextInput/InputGroupBase";
+import InputGroupText from "../TextInput/InputGroupTextBase";
+
+const ImsInputSelectWithIcon = ({
+  label,
+  error,
+  icon,
+  name,
+  onChange = () => {},
+  mandatory = false,
+  ...rest
+}) => {
+  const handleChange = (changes) => {
+    if (!changes) {
+      let currentTarget = {
+        name,
+        value: [],
+      };
+      onChange({ currentTarget });
+    } else if (Array.isArray(changes)) {
+      let currentTarget = {
+        name,
+        value: changes, //changes.map(item => item.value)
+      };
+      onChange({ currentTarget });
+    } else {
+      let currentTarget = {
+        name,
+        value: changes, //changes.value
+      };
+      onChange({ currentTarget });
+    }
+  };
+  return (
+    <>
+      <FormGroup row>
+        <Col sm="12">
+          <Label>
+            {label} {mandatory ? <span className="text-danger">*</span> : ""}
+          </Label>
+        </Col>
+        <Col sm="12">
+          <InputGroup>
+            <InputGroupText>
+              <i className={`${icon}`} />
+            </InputGroupText>
+            <Select {...rest} onChange={handleChange} />
+          </InputGroup>
+          {error && <label className="text-danger">{error}</label>}
+        </Col>
+      </FormGroup>
+    </>
+  );
+};
+
+export default ImsInputSelectWithIcon;
