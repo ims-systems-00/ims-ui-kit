9476e6f9b4731487fc81ce6a1833133f7586ff29
RiadHossain43 <riadhossain7464@gmail.com>
2023-02-22 18:31:49 +0600

    latest

--- src/components/Activities/ActivityBase.jsx
+++ src/components/Activities/ActivityBase.jsx
@@ -13,25 +13,26 @@ const ActivityBase = ({
   ...rest
 }) => {
   return (
-    <Card className="bg-transparent border-0 shadow-none comment ms-5 ml-5 mb-0">
-      <CardHeader className="bg-transparent border-0 shadow-none">
-        <div className="d-flex">
-          <div className="avatar-container">
-            <img src={avatar} alt="avatar" className="avatar mt-1 ms-3 ml-3" />
-          </div>
-          <div>
-            <p className="m-0">{name}</p>
-            {subHeading && <small className="mb-1">{subHeading}</small>}
-          </div>
-        </div>
-      </CardHeader>
-      <CardBody className="bg-transparent border-0 shadow-none pt-2 ">
-        {activity}
-      </CardBody>
-      <CardFooter className="bg-transparent border-0 shadow-none pt-0">
-        {footer}
-      </CardFooter>
-    </Card>
+    // <Card className="bg-transparent border-0 shadow-none comment ms-5 ml-5 mb-0">
+    //   <CardHeader className="bg-transparent border-0 shadow-none">
+    //     <div className="d-flex">
+    //       <div className="avatar-container">
+    //         <img src={avatar} alt="avatar" className="avatar mt-1 ms-3 ml-3" />
+    //       </div>
+    //       <div>
+    //         <p className="m-0">{name}</p>
+    //         {subHeading && <small className="mb-1">{subHeading}</small>}
+    //       </div>
+    //     </div>
+    //   </CardHeader>
+    //   <CardBody className="bg-transparent border-0 shadow-none pt-2 ">
+    //     {activity}
+    //   </CardBody>
+    //   <CardFooter className="bg-transparent border-0 shadow-none pt-0">
+    //     {footer}
+    //   </CardFooter>
+    // </Card>
+    <Card></Card>
   );
 };
 
