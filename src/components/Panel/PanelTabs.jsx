c663a6b0a193e262a39fa44acba65df17333e85d
Samin Israr Ravi <saminisrar1@gmail.com>
2023-04-04 22:47:34 +0600

    add: variant prop

--- src/components/Panel/PanelTabs.jsx
+++ src/components/Panel/PanelTabs.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Nav } from "../../index";
 
-const PanelTabs = ({ children, onChange, value, ...props }) => {
+const PanelTabs = ({ children, onChange, value, variant, ...props }) => {
   return (
     <React.Fragment>
       <div className="panel-tabs">
-        <Nav tabs>
+        <Nav tabs variant={variant && variant}>
           {React.Children.map(children, (child, index) => {
             return React.cloneElement(child, {
               onClick: (e) => onChange(e, index),
