ade87884800670578b7f94ecf75d8d2293909559
Samin Israr Ravi <saminisrar1@gmail.com>
2023-04-05 16:30:06 +0600

    add: table filters fixed

--- src/components/Table/AdvancedTable/FilterAndSearch.jsx
+++ src/components/Table/AdvancedTable/FilterAndSearch.jsx
@@ -18,6 +18,7 @@ const FilterAndSearch = ({
   createBtn = null,
   title = "",
 }) => {
+  let [filterLabel, setFilterLabel] = React.useState("");
   let [searchString, setSearchString] = React.useState("");
   const debouncedSearchString = useDebounce(searchString, 500);
   const [dropdownOpen, setDropdownOpen] = React.useState(false);
@@ -27,7 +28,7 @@ const FilterAndSearch = ({
   }, [debouncedSearchString]);
   return (
     <React.Fragment>
-      <Row className="filter-search d-lg-flex justify-around align-items-center px-lg-4 py-4">
+      <Row className="filter-search d-lg-flex justify-around align-items-center px-lg-4 my-4">
         <Col md="4" sm="12">
           <h3>{title}</h3>
         </Col>
@@ -35,9 +36,9 @@ const FilterAndSearch = ({
           <div className="d-flex justify-content-lg-end justify-content-md-end action-container">
             <div className="d-flex create-filter-wrapper">
               {createBtn && (
-                <div className="pe-3 create-filer">{createBtn}</div>
+                <div className="me-3 mr-3 create-filer">{createBtn}</div>
               )}
-              <div className="pe-3 create-filer">
+              <div className="me-3 mr-3 create-filer">
                 <Dropdown
                   onChange={(value) => {
                     onFilter(value);
@@ -52,20 +53,21 @@ const FilterAndSearch = ({
                     outline
                     size="md"
                   >
-                    <i class="fa-solid fa-filter pe-2 py-0 shadow-sm--hover" />
-                    Filter
+                    <i class="fa-solid fa-filter me-2 mr-3  shadow-sm--hover" />
+                    {filterLabel ? filterLabel : "Filter"}
                   </DropdownToggle>
                   <DropdownMenu>
                     {filters &&
                       filters.length > 0 &&
-                      filters.map((item) => (
+                      filters.map((filter) => (
                         <DropdownItem
                           onClick={() => {
-                            onFilter(item);
+                            setFilterLabel(filter.label);
+                            onFilter(filter);
                           }}
                           className="d-flex align-items-center fw-bold my-2"
                         >
-                          {item}
+                          {filter.label}
                         </DropdownItem>
                       ))}
                   </DropdownMenu>
