90993239e8d5c272c5614974e8b272102150aa29
Samin Israr Ravi <saminisrar1@gmail.com>
2023-04-18 02:48:17 +0600

    add: dashboard carousel

---
+++ src/components/DashboardCarousel/Example/DashboardCarouselExample.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Card, CardBody } from '../../../index';
+import DashboardCarousel from '../DashboardCarousel';
+
+const DashboardCarouselExample = ({}) => {
+  return (
+    <DashboardCarousel>
+      <Card>
+        <CardBody>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore
+          neque quasi ex, dignissimos accusamus est, itaque, deserunt
+          voluptatibus odio praesentium iure? Commodi reiciendis at eveniet.
+        </CardBody>
+      </Card>
+      <Card>
+        <CardBody>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore
+          neque quasi ex, dignissimos accusamus est, itaque, deserunt
+          voluptatibus odio praesentium iure? Commodi reiciendis at eveniet.
+        </CardBody>
+      </Card>
+      <Card>
+        <CardBody>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore
+          neque quasi ex, dignissimos accusamus est, itaque, deserunt
+          voluptatibus odio praesentium iure? Commodi reiciendis at eveniet.
+        </CardBody>
+      </Card>
+      <Card>
+        <CardBody>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore
+          neque quasi ex, dignissimos accusamus est, itaque, deserunt
+          voluptatibus odio praesentium iure? Commodi reiciendis at eveniet.
+        </CardBody>
+      </Card>
+      <Card>
+        <CardBody>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore
+          neque quasi ex, dignissimos accusamus est, itaque, deserunt
+          voluptatibus odio praesentium iure? Commodi reiciendis at eveniet.
+        </CardBody>
+      </Card>
+      <Card>
+        <CardBody>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore
+          neque quasi ex, dignissimos accusamus est, itaque, deserunt
+          voluptatibus odio praesentium iure? Commodi reiciendis at eveniet.
+        </CardBody>
+      </Card>
+    </DashboardCarousel>
+  );
+}
+
+export default DashboardCarouselExample
