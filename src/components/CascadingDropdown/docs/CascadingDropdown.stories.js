c6c2c67bc3fa0441e7ab0222b6953bc104c2a4f9
Bobi <devbobi505@gmail.com>
2023-04-19 23:34:59 +0600

    cascading dropdown initiated

---
+++ src/components/CascadingDropdown/docs/CascadingDropdown.stories.js
@@ -0,0 +1,18 @@
+// import Accordion from "../Examples/DefaultAccordion";
+import CascadingDropdown from "../CascadingDropdown";
+export default {
+  title: "components/CascadingDropdown",
+  component: CascadingDropdown,
+  parameters: {
+    docs: {
+      description: {
+        component: `
+[Cascading Dropdown](https://getbootstrap.com/docs/5.3/components/dropdown/)
+All our Cascading Dropdown are customised from the default bootstrap dropdown. 
+`,
+      },
+    },
+  },
+};
+// export { default as Accordion } from "../Examples/DefaultAccordion";
+export { default as CascadingDropdown } from "../CascadingDropdown";
