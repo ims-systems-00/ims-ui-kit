70fe8de67e76d45f8623670ecc66abc034677ca4
RiadHossain43 <riadhossain7464@gmail.com>
2024-05-14 16:07:46 +0600

    input filled variant added to ui

--- src/components/Forms/Inputs/InputBase.jsx
+++ src/components/Forms/Inputs/InputBase.jsx
@@ -1,3 +1,5 @@
+import classNames from "classnames";
+import PropTypes from "prop-types";
 import { Input } from "reactstrap";
 
 let InputBaseProps = Object.assign({}, Input.prototype.props);
@@ -7,12 +9,29 @@ let InputBaseProps = Object.assign({}, Input.prototype.props);
  * @param {InputBaseProps} props
  * @returns
  */
-function InputBase(props) {
+
+const variants = {
+  outline: "outline",
+  filled: "filled",
+};
+function InputBase({ variant = variants.outline, ...props }) {
+  if (variant !== variants.filled)
+    return (
+      <Input
+        className={classNames(props.className || "", {
+          "form-control-filled": true,
+        })}
+        {...props}
+      >
+        {props.children}
+      </Input>
+    );
   return <Input {...props}>{props.children}</Input>;
 }
 
 InputBase.propTypes = {
   ...(Input.propTypes && Input.propTypes),
+  variant: PropTypes.oneOf(...Object.values(variants)),
 };
 
 export default InputBase;
