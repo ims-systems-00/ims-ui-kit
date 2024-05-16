2dac06ad0fe5cece853e81a787cb4777bfe51c4a
RiadHossain43 <riadhossain7464@gmail.com>
2024-05-16 16:37:48 +0600

    filled date and time component shipped and estabilished

--- src/components/Forms/DateTimePicker/DateTimeBase.jsx
+++ src/components/Forms/DateTimePicker/DateTimeBase.jsx
@@ -1,8 +1,20 @@
+import classNames from "classnames";
 import Datetime from "react-datetime";
 import "react-datetime/css/react-datetime.css";
 
-const DateTimeBase = ({...rest }) => {
-  return <Datetime {...rest} />;
+const variants = {
+  outline: "outline",
+  filled: "filled",
+};
+const DateTimeBase = ({ variant = variants.outline, ...rest }) => {
+  return (
+    <Datetime
+      className={classNames(rest.className || "", {
+        "form-control-filled": variant !== variants.filled,
+      })}
+      {...rest}
+    />
+  );
 };
 
 export default DateTimeBase;
