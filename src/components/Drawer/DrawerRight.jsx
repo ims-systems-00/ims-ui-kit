a3b64d73b5f435c518341756f4a8cb184741c94f
RiadHossain43 <riadhossain7464@gmail.com>
2024-06-21 17:32:42 +0600

    icon of drawer reduced to 16px

--- src/components/Drawer/DrawerRight.jsx
+++ src/components/Drawer/DrawerRight.jsx
@@ -76,9 +76,9 @@ const DrawerRight = ({
                   size="sm"
                 >
                   <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
                   >
