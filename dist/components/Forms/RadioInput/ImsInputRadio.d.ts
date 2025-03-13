22a8c0df742e130754df3fe43922227fc8f68dc7
Gm-shuvo <145977738+gmshuvo@users.noreply.github.com>
2025-03-13 14:16:30 +0600

    bootstrap issue unsloved

--- dist/components/Forms/RadioInput/ImsInputRadio.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { InputBaseProps } from "../Inputs/InputBase";
-interface ImsInputRadioProps extends InputBaseProps {
-    label: string;
-    mandatory?: boolean;
-    unselected?: boolean;
-}
-declare const ImsRadioCheckbox: React.FC<ImsInputRadioProps>;
-export default ImsRadioCheckbox;
